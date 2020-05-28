db9eabcfe854492f570926223a282feeb4c4fa21
WH-01 <2665423593@qq.com>
2020-05-28 19:49:02 +0800

    程序员B完成任务

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello Git!");//
         System.out.println("我是程序员");
         System.out.println("我是程序员A，我已完成程序修改");
+        System.out.println("我是程序员B，我已完成任务");
     }
 }
