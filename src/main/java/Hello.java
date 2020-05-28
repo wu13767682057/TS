f37e73ab039a04c6bafb85310eb80b72a09abbd2
WH-01 <2665423593@qq.com>
2020-05-28 19:43:38 +0800

    程序员A完成修改

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String args[]){
         System.out.println("Hello Git!");//
         System.out.println("我是程序员");
+        System.out.println("我是程序员A，我已完成程序修改");
     }
 }
